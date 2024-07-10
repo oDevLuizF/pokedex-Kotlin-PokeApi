dd37b970ac6b8c0ef54a4a97f2d4defff23652e3
oDevLuizF <145712801+oDevLuizF@users.noreply.github.com>
2024-07-09 22:29:22 -0300

    Feat: add recycler view & pokemon-card

--- app/src/main/java/com/example/pokedexs1/MainActivity.kt
+++ app/src/main/java/com/example/pokedexs1/MainActivity.kt
@@ -15,29 +15,9 @@ import com.example.pokedexs1.ui.theme.PokedexS1Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            PokedexS1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
+        setContentView(R.layout.activity_main)
+
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PokedexS1Theme {
-        Greeting("Android")
-    }
-}
